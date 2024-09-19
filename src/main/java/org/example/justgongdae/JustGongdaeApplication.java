60d2a37ed75dc0be69c535b22efb050558f37552
unknown <judemin2087@naver.com>
2024-09-19 21:53:49 +0900

    [INIT] : Initialized project just_gongdae

---
+++ src/main/java/org/example/justgongdae/JustGongdaeApplication.java
@@ -0,0 +1,13 @@
+package org.example.justgongdae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGongdaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustGongdaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/justgongdae/JustGongdaeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.justgongdae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustGongdaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
